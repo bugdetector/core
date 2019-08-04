bd20d6ef56ed0915172eecc6c06adba6dd0731e8
bugdetector <bakiyucel38@gmail.com>
2019-08-04 23:50:48 +0300

    File input correction

--- kernel/database/CoreDB.php
+++ kernel/database/CoreDB.php
@@ -386,3 +386,16 @@ function get_all_table_references() {
 function is_unique_foreign_key(string $table, string $uni) {
     return get_foreign_key_description($table, $uni)->rowCount() !== 0;
 }
+
+function get_file_input(string $file_field, string $file_name = "") {
+    return
+    "<div >
+        <div class='btn btn-success col-sm-2 col-xs-12 file-field'>
+            "._t(111)."
+        </div>
+        <input type='file' name='$file_field' style='display: none;'/>
+        <div class='col-sm-10 col-xs-12'>
+            <input class='file-path form-control' type='text' value='$file_name' placeholder='"._t(112)."'/>
+        </div>
+    </div>";
+}
