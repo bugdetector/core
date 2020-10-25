cc4f0cdc929ab6a4ac25460b76789f37462f9592
bugdetector <bakiyucel38@gmail.com>
2020-10-25 23:01:43 +0300

    base theme frontend files moved under base theme directory and dist directory add to git Readme update

---
+++ public_html/dist/table_and_column_selector/table_and_column_selector.js
@@ -0,0 +1,113 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "../../";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 13);
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./base_theme/src/views/table_and_column_selector.js":
+/*!***********************************************************!*\
+  !*** ./base_theme/src/views/table_and_column_selector.js ***!
+  \***********************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+$(function(){$(document).on("change",".table_select",function(){var select=$(this);if(!select.val()){return;}var column_select=select.parents(".table_and_column_selector").find("select.column_select");var type=column_select.data("type");$.ajax({url:"".concat(root,"/admin/ajax/getTableColumns"),method:"post",dataType:"json",data:{table:select.val(),type:type},success:function success(response){column_select.html("");for(var column in response.data){var option=$("<option>");option.val(column).text(response.data[column]);column_select.append(option);}selectpicker(column_select,"refresh");}});});$(document).on("click",".removefilter",function(e){e.preventDefault();$(this).parents(".table_and_column_selector").fadeOut(500).delay(500).queue(function(){$(this).remove();});});$(".new_filter, .new_field").on("click",function(e){e.preventDefault();var button=$(this);var type=button.data("type");$.ajax({url:"".concat(root,"/admin/ajax/getTableAndColumnSelector"),method:"post",data:{index:button.parents(".filters, .fields").find(".table_and_column_selector").length,type:type,name:button.data("name")},success:function success(response){var row=$(response);button.before(row);selectpicker(row.find(".selectpicker"));}});});});
+
+/***/ }),
+
+/***/ 13:
+/*!*****************************************************************!*\
+  !*** multi ./base_theme/src/views/table_and_column_selector.js ***!
+  \*****************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(/*! ./base_theme/src/views/table_and_column_selector.js */"./base_theme/src/views/table_and_column_selector.js");
+
+
+/***/ })
+
+/******/ });
+//# sourceMappingURL=table_and_column_selector.js.map
