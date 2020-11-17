5a6fda735da2215502854a960c60be44f9536fa1
bugdetector <bakiyucel38@gmail.com>
2020-11-17 10:15:15 +0300

    summetnote backdrop fix wrapper min-heinght fix

--- public_html/base_theme/src/components/summernote/summernote.js
+++ public_html/base_theme/src/components/summernote/summernote.js
@@ -2,7 +2,7 @@ import {$} from "../jquery/jquery";
 import "bootstrap/js/src/tooltip";
 import 'summernote/dist/summernote-bs4';
 import 'summernote/dist/summernote-bs4.css';
-
+import "./summernote.scss";
 $(function ($) {
     $('.summernote').summernote();
 })
