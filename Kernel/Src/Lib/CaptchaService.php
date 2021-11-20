e9e078a4025207e42a9a9f51c846816603227bd1
Murat Baki Yücel <bakiyucel38@gmail.com>
2021-11-20 18:33:52 +0300

    #715 Captcha integration library - code fix

--- Kernel/Src/Lib/CaptchaService.php
+++ Kernel/Src/Lib/CaptchaService.php
@@ -35,23 +35,42 @@ class CaptchaService
         $green = rand(125, 175);
         $blue = rand(125, 175);
         for ($i = 0; $i < 5; $i++) {
-            $colors[] = imagecolorallocate($image, $red - 20 * $i, $green - 20 * $i, $blue - 20 * $i);
+            $colors[] = imagecolorallocate(
+                $image,
+                $red - 20 * $i,
+                $green - 20 * $i,
+                $blue - 20 * $i
+            );
         }
         imagefill($image, 0, 0, $colors[0]);
         for ($i = 0; $i < 10; $i++) {
             imagesetthickness($image, rand(2, 10));
             $rect_color = $colors[rand(1, 4)];
-            imagerectangle($image, rand(-10, 190), rand(-10, 10), rand(-10, 190), rand(40, 60), $rect_color);
+            imagerectangle(
+                $image,
+                rand(-10, 190),
+                rand(-10, 10),
+                rand(-10, 190),
+                rand(40, 60),
+                $rect_color
+            );
         }
         $black = imagecolorallocate($image, 0, 0, 0);
         $white = imagecolorallocate($image, 255, 255, 255);
         $textcolors = [$black, $white];
         
         $captchaString = $this->generateCaptchaString($this->lenght);
+        $initial = 15;
+        $letterSpace = 170 / $this->lenght;
         for ($i = 0; $i < $this->lenght; $i++) {
-            $letter_space = 170 / $this->lenght;
-            $initial = 15;
-            imagestring($image, 20, $initial + $i * $letter_space, rand(10, 30), $captchaString[$i], $textcolors[rand(0, 1)]);
+            imagestring(
+                $image,
+                20,
+                $initial + $i * $letterSpace,
+                rand(10, 30),
+                $captchaString[$i],
+                $textcolors[rand(0, 1)]
+            );
         }
         return $image;
     }
