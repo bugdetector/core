ae0c894e5a4fc331d1b3e3cb8a4ace569c704177
Murat Baki Yücel <bakiyucel38@gmail.com>
2022-03-07 00:30:43 +0300

    Tree form fix

--- public_html/assets/js/components/collapsable_card.js
+++ public_html/assets/js/components/collapsable_card.js
@@ -1,19 +1,14 @@
 $(function ($) {
-    var list = $(".sortable_list");
-    if (list.find(".sortable").length > 0) {
-        for (var item of list) {
-            new Sortable.default(item, {
-                animation: 100,
-                group: 'sortable_list',
-                draggable: '.sortable',
-                handle: '.sortable .move_icon',
-                sort: true,
-                filter: '.sortable-disabled',
-                mirror: {
-                    appendTo: 'body',
-                    constrainDimensions: true
-                }
-              });
+    new Sortable.default($(".sortable_list").toArray(), {
+        animation: 100,
+        group: 'sortable_list',
+        draggable: '.sortable',
+        handle: '.sortable .move_icon',
+        sort: true,
+        filter: '.sortable-disabled',
+        mirror: {
+            appendTo: 'body',
+            constrainDimensions: true
         }
-    }
+    });
 })
