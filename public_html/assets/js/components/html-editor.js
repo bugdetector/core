684c203d4c257007d85f5a605775e70473d5830d
Murat Baki Yücel <bakiyucel38@gmail.com>
2022-03-07 14:35:54 +0300

    html editor fix

--- public_html/assets/js/components/html-editor.js
+++ public_html/assets/js/components/html-editor.js
@@ -20,8 +20,24 @@ $(function(){
             },
             theme: 'snow' // or 'bubble'
         });
+        let toolbar = editor.getModule("toolbar");
+        toolbar.addHandler("code-block", function(value){
+            let htmlContent = $("<textarea>").addClass("form-control bg-dark text-light min-h-200px").val(editor.root.innerHTML);
+            [modal, modalContent] = openModal(
+                "HTML",
+                htmlContent,
+                `<button type="button" class="btn btn-danger btn-sm" data-bs-dismiss="modal">${_t("cancel")}</button>
+                <button type="button" class="btn btn-primary btn-sm save-code-block">${_t("save")}</button>`,
+                "modal-lg"
+            );
+            modalContent.on("click", ".save-code-block", function(){
+                editor.root.innerHTML = htmlContent.val();
+                textarea.val(editor.root.innerHTML);
+                modal.hide();
+            })
+        });
         editor.on('text-change', function(delta, oldDelta, source) {
-            textarea.val(editor.getText());
+            textarea.val(editor.root.innerHTML);
         })
         return editor;
     }
