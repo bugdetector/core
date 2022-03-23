60b3d4610058ea96e70b112c33eb93029284d225
Murat Baki Yücel <bakiyucel38@gmail.com>
2022-03-23 18:20:47 +0300

    #FU-58 - Make description more visiable

--- Kernel/Src/Form/Widget/TextareaWidget.php
+++ Kernel/Src/Form/Widget/TextareaWidget.php
@@ -20,7 +20,11 @@ class TextareaWidget extends FormWidget
     public function addClass(string $class_name): View
     {
         if (in_array("html-editor", explode(" ", $class_name))) {
-            \CoreDB::controller()->addJsFiles("assets/js/components/html-editor.js");
+            $controller = \CoreDB::controller();
+            $controller->addJsFiles([
+                "base_theme/assets/plugins/custom/tinymce/tinymce.bundle.js",
+                "assets/js/components/html-editor.js"
+            ]);
         }
         return parent::addClass($class_name);
     }
