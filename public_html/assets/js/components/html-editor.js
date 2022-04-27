2710d4d0cd583323ce124e85e1eaafa64feb71a1
bugdetector <bakiyucel38@gmail.com>
2022-04-27 16:09:30 +0300

    Html editor context menu fix

--- public_html/assets/js/components/html-editor.js
+++ public_html/assets/js/components/html-editor.js
@@ -3,13 +3,15 @@ $(function(){
         return tinymce.init({
             target: element,
             toolbar: ['styleselect fontselect fontsizeselect',
-                'undo redo | cut copy paste | bold italic forecolor backcolor | link image | alignleft aligncenter alignright alignjustify | bullist numlist | outdent indent | blockquote | code'],
-            plugins : 'autosave link image lists code',
+                'undo redo | cut copy paste | bold italic forecolor backcolor | link image | alignleft aligncenter alignright alignjustify | bullist numlist | table | outdent indent | blockquote | code'],
+            plugins : 'autosave link image table lists code',
+            contextmenu: 'cut copy paste',
             branding: false,
             resize: true,
             min_height: 500,
             relative_urls: false,
             remove_script_host: false,
+            forced_root_block: false,
             setup: function(editor) {
                 editor.on('change keyup input', function () {
                     editor.save();
