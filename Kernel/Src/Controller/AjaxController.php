5cc146b476aa5a045809c347204ef58947b878f5
bugdetector <bakiyucel38@gmail.com>
2022-06-30 13:30:06 +0300

    #CDB-15 - Multiple File Input Widget

--- Kernel/Src/Controller/AjaxController.php
+++ Kernel/Src/Controller/AjaxController.php
@@ -6,7 +6,6 @@ use CoreDB\Kernel\Messenger;
 use CoreDB\Kernel\ServiceController;
 use CoreDB\Kernel\Model;
 use Exception;
-use Src\BaseTheme\BaseTheme;
 use Src\Entity\File;
 use Src\Entity\Translation;
 use Src\Form\Widget\CollapsableWidgetGroup;
@@ -103,6 +102,39 @@ class AjaxController extends ServiceController
                 $this->response_type = self::RESPONSE_TYPE_RAW;
                 echo \Src\Theme\CoreRenderer::getInstance()
                 ->renderWidget($fileWidget);
+            }
+        } catch (Exception $ex) {
+            throw new Exception($ex->getMessage());
+        }
+    }
+
+    public function uploadFile()
+    {
+        $key = @$_POST["key"];
+        try {
+            $jwt = JWT::createFromString($key);
+            $data = $jwt->getPayload();
+            $referenceClass = \CoreDB::config()->getEntityInfo($data->entity)["class"];
+            /** @var Model */
+            $object = $referenceClass::get($data->id) ?: new $referenceClass();
+            if (@$data->field) {
+                $file = new File();
+                $file->storeUploadedFile($object->getTableName(), $data->field, $_FILES["file"]);
+                $file->status->setValue(File::STATUS_TEMPORARY);
+                $file->save();
+                $response = $file->toArray();
+                $response["ID"] = $file->ID->getValue();
+                $response["file_size"] = $file->sizeConvertToString($response["file_size"]);
+                $response["is_image"] = $file->isImage;
+
+                $removeKeyJwt = new JWT();
+                $removeKeyJwt->setPayload([
+                    "entity" => $file->entityName,
+                    "id" => $file->ID->getValue()
+                ]);
+                $response["remove_key"] = $removeKeyJwt->createToken();
+                $response["icon_class"] = $file->getFileIconClass();
+                return $response;
             }
         } catch (Exception $ex) {
             throw new Exception($ex->getMessage());
---
+++ Kernel/Src/Form/Widget/MultipleFileInputWidget.php
@@ -0,0 +1,160 @@
+<?php
+
+namespace Src\Form\Widget;
+
+use CoreDB;
+use CoreDB\Kernel\Database\DataType\File;
+use CoreDB\Kernel\EntityReference;
+use CoreDB\Kernel\Model;
+use Exception;
+use Src\Entity\File as EntityFile;
+use Src\Entity\Translation;
+use Src\JWT;
+use Src\Views\Image;
+use Src\Views\Link;
+use Src\Views\Table;
+use Src\Views\TextElement;
+use Src\Views\ViewGroup;
+
+class MultipleFileInputWidget extends FormWidget
+{
+    public Model $baseObject;
+    public $referenceClass;
+    public string $referenceClassEntityName;
+    public JWT $fileKeyJwt;
+
+    /**
+     * @param string $name
+     *  Input name
+     * @param Model $baseObject
+     *  Base object for file references.
+     * @param string $referenceClass
+     *  Model class path for file attachment.
+     */
+    public function __construct(string $name, Model $baseObject, $referenceClass)
+    {
+
+        $this->setName($name);
+        $this->setBaseObject($baseObject);
+        $this->setReferenceClass($referenceClass);
+        parent::__construct("{$this->baseObject->entityName}[{$this->referenceClassEntityName}]");
+
+        $this->fileKeyJwt = new JWT();
+        $this->fileKeyJwt->setPayload([
+            "entity" => $this->referenceClassEntityName,
+            "id" => null,
+            "field" => $this->getFileFieldNameOfReferenceClass()
+        ]);
+        $controller = CoreDB::controller();
+        $controller->addJsFiles([
+            "assets/js/widget/multiple-file-input-widget.js"
+        ]);
+    }
+
+    public function setBaseObject(Model $baseObject)
+    {
+        $this->baseObject = $baseObject;
+    }
+
+    public function setReferenceClass($referenceClass)
+    {
+        $this->referenceClass = $referenceClass;
+        $this->referenceClassEntityName = $this->getReferenceClassEntityName();
+    }
+
+    protected function getReferenceClassEntityName()
+    {
+        $entityInfo = CoreDB::config()->getEntityInfoByClass($this->referenceClass);
+        if (!$entityInfo) {
+            throw new Exception("Entity configuration not provided for reference class.");
+        }
+        return key($entityInfo);
+    }
+
+    protected function getFiles(): array
+    {
+        $entityName = $this->referenceClassEntityName;
+        /** @var EntityReference */
+        $fileField = $this->baseObject->{$entityName};
+        return $fileField->getCheckeds();
+    }
+
+    protected function getFileFieldNameOfReferenceClass()
+    {
+        $referenceClassInstance = new $this->referenceClass();
+        foreach ($referenceClassInstance as $fieldName => $field) {
+            if ($field instanceof File) {
+                return $fieldName;
+            }
+        }
+        throw new Exception("Reference class is not have any File field.");
+    }
+
+    public function getFilesTable(): Table
+    {
+        $table = new Table();
+        $table->addClass("multiple-files-content")
+        ->addAttribute("data-name", $this->name);
+        $table->setHeaders([
+            "",
+            Translation::getTranslation("file_name"),
+            Translation::getTranslation("file_size"),
+            "",
+        ]);
+        $referenceClassEntityName = $this->referenceClassEntityName;
+        $fileFieldName = $this->getFileFieldNameOfReferenceClass();
+        $data = [];
+        foreach ($this->getFiles() as $index => $fileInfo) {
+            /** @var EntityFile */
+            $file = EntityFile::get($fileInfo->{$fileFieldName}->getValue());
+            $row = [];
+            if ($file->isImage) {
+                $row[0] = ViewGroup::create("div", "mw-50px")
+                ->addField(
+                    Link::create(
+                        "#",
+                        Image::create($file->getUrl(), $file->file_name)
+                        ->addClass("img-thumbnail ms-2")
+                    )->addClass("image-preview")
+                    ->addAttribute("data-field-name", Translation::getTranslation($fileFieldName))
+                );
+            } else {
+                $row[0] = ViewGroup::create("div", "mw-50px")
+                ->addField(
+                    ViewGroup::create("span", $file->getFileIconClass() . " fs-2x ms-2 text-primary")
+                );
+            }
+            $row[0]->addField(
+                InputWidget::create($this->name . "[{$index}][{$fileFieldName}]")
+                ->setType("hidden")
+                ->setValue($file->ID->getValue())
+            );
+            $row[] = Link::create(
+                $file->getUrl(),
+                $file->file_name
+            );
+            $row[] = $file->sizeConvertToString($file->file_size->getValue());
+            $removeKeyJwt = new JWT();
+            $removeKeyJwt->setPayload([
+                "entity" => $referenceClassEntityName,
+                "id" => $fileInfo->ID->getValue()
+            ]);
+            $row[] = Link::create(
+                "#",
+                TextElement::create(
+                    "<span class='fa fa-trash'></span>"
+                )->setIsRaw(true)
+            )->addClass("btn btn-sm btn-light-danger multiple-file-delete")
+            ->addAttribute("data-key", $removeKeyJwt->createToken())
+            ->addAttribute("data-field-name", Translation::getTranslation($fileFieldName));
+            $data[] = $row;
+        }
+        $table->setData($data);
+        return $table;
+    }
+
+    public function getTemplateFile(): string
+    {
+        return "multiple-file-input-widget.twig";
+    }
+}
